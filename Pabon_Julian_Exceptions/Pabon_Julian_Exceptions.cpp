ca8bb0513d40073ba39811476b272400c57d3237
Julian Pabon <julianps101@hotmail.com>
2021-05-03 01:28:33 -0400

    Added interface between user and function

--- Pabon_Julian_Exceptions/Pabon_Julian_Exceptions.cpp
+++ Pabon_Julian_Exceptions/Pabon_Julian_Exceptions.cpp
@@ -5,6 +5,23 @@
 #include <iostream>
 using namespace std;
 
+//function prototype
+char character(char start, int offset);
+
 int main()
+{
+	//declare variables
+	char start;
+	int offset;
+	//get input
+	cout << "Enter starting character: ";
+	cin >> start;
+	cout << "Enter amount of offset: ";
+	cin >> offset;
+	//call function and output it
+	character(start, offset);
+}
+
+char character(char start, int offset)
 {
 }
