bb90982846983e5fa4e7426186b91b92e878516e
Julian Pabon <julianps101@hotmail.com>
2021-05-03 01:45:05 -0400

    Added character functionality

--- Pabon_Julian_Exceptions/Pabon_Julian_Exceptions.cpp
+++ Pabon_Julian_Exceptions/Pabon_Julian_Exceptions.cpp
@@ -23,5 +23,30 @@ int main()
 }
 
 char character(char start, int offset)
-{
+{ 
+	//test if start is letter
+	try 
+	{
+		if (start < 'A' || start < 'z')
+		{
+			throw start;
+		}
+	}
+	catch (char startException)
+	{
+		cout << "invalidCharacterExcpeption: " << startException << " is not a letter\n";
+	}
+	//try if start + offset is a letter
+	try
+	{
+		if ((start + offset) < 'A' || (start + offset) < 'z')
+		{
+			throw (start + offset);
+		}
+	}
+	catch (int offsetException)
+	{
+		cout << "invalidRangeException: " << offsetException << " is not a letter\n";
+	}
 }
+
